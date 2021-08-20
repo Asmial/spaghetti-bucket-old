66b32a6fd4a8adb742da9b9689ee63bd6329d035
Asmial <asiermiguel@gmail.com>
2021-08-20 18:14:54 +0200

    generic script for any program

---
+++ scripts/shortcuts-fix.ps1
@@ -0,0 +1,9 @@
+@(arch_specific 'shortcuts' $manifest $arch) | Where-Object { $_ -ne $null } | ForEach-Object {
+    $directory = shortcut_folder $global
+    $target = (scoop prefix $app | Get-Item).Target
+    $shell = New-Object -COM WScript.Shell
+    $shortcut = $shell.CreateShortcut("$directory\\$($_.item(1)).lnk")
+    $shortcut.TargetPath = "$target\$($_.item(0))"
+    $shortcut.Save()
+    shim $shortcut.TargetPath $false $name (substitute $arg @{ '$dir' = $dir; '$original_dir' = $original_dir; '$persist_dir' = $persist_dir })
+}
