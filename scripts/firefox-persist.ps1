7f52435926d456d5dd87ebe016db6d5546b7d7be
Asmial <asiermiguel@gmail.com>
2021-09-14 18:49:56 +0200

    fix persist

--- scripts/firefox-persist.ps1
+++ scripts/firefox-persist.ps1
@@ -1,6 +1,6 @@
+New-Item -Path "$env:APPDATA\Mozilla" -ItemType Directory -ErrorAction Ignore 
+New-Item -Path "$env:APPDATA\Mozilla\Firefox" -ItemType Directory -ErrorAction Ignore
 if(!(Test-Path "$persist_dir\Mozilla\Firefox")){
-    New-Item -Path "$env:APPDATA\Mozilla" -ItemType Directory -ErrorAction Ignore
-    New-Item -Path "$env:APPDATA\Mozilla\Firefox" -ItemType Directory -ErrorAction Ignore
-    Move-Item -Path "$env:APPDATA\Mozilla\Firefox" -Destination "$persist_dir\Mozilla"  -Force  
+    Move-Item -Path "$env:APPDATA\Mozilla\Firefox" -Destination "$persist_dir\Mozilla"  -Force  | out-null
 }
 & "$env:COMSPEC" /c mklink /j "$env:APPDATA\Mozilla\Firefox" "$persist_dir\Mozilla\Firefox" | out-null
